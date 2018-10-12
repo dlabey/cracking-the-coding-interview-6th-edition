830c44492afa3fe3c2e9f7ccec0b8f31f5f6d653
Darren Labey <dlabey@fastmail.com>
2018-10-11 22:25:36 -0700

    Solution for 1.1

---
+++ src/org/cracking_the_coding_interview/chapter_1/is_unique/Main.java
@@ -0,0 +1,54 @@
+package org.cracking_the_coding_interview.chapter_1.is_unique;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static boolean isUnique(String str) {
+        List<Character> chars = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char character = str.charAt(i);
+
+            if (chars.contains(character)) {
+                return false;
+            } else {
+                chars.add(character);
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isUniqueNoDataStructure(String str) {
+        int checker = 0;
+
+        for (int i = 0; i < str.length(); ++i) {
+            int val = str.charAt(i) - 'a';
+
+            if ((checker & (1 << val)) > 0) {
+                return false;
+            }
+            checker |= (1 << val);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+	    String[] strings = new String[]{"teeth", "sharks", "coding", "dad"};
+
+	    System.out.println("isUnique:");
+	    for (String string:strings) {
+            System.out.println(String.format("%1$s: %2$b", string, isUnique(string)));
+        }
+
+        System.out.println();
+
+        System.out.println("isUniqueNoDataStructure:");
+        for (String string:strings) {
+            System.out.println(String.format("%1$s: %2$b", string, isUniqueNoDataStructure(string)));
+        }
+    }
+}
