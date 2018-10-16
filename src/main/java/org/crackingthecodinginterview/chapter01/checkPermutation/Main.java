5fa83ba76824df43c6e1ad2298a527f6f65e60ea
Darren Labey <dlabey@fastmail.com>
2018-10-15 22:35:23 -0700

    Adding isPermutation check using sorted string

--- src/main/java/org/crackingthecodinginterview/chapter01/checkPermutation/Main.java
+++ src/main/java/org/crackingthecodinginterview/chapter01/checkPermutation/Main.java
@@ -1,5 +1,7 @@
 package main.java.org.crackingthecodinginterview.chapter01.checkPermutation;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -9,16 +11,24 @@ public class Main {
             return false;
         }
 
-        str1 = str1.chars().sorted().toString();
-        str2 = str2.chars().sorted().toString();
+        char[] str1Array = str1.toCharArray();
+        char[] str2Array = str2.toCharArray();
+
+        Arrays.sort(str1Array);
+        Arrays.sort(str2Array);
+        str1 = String.join("", new String(str1Array));
+        str2 = String.join("", new String(str2Array));
 
         return str1.equals(str2);
     }
 
     public static void main(String[] args) {
-        System.out.println("isPermutation:");
+        System.out.println("isPermutationSortedString:");
         String str1 = "test1";
         String str2 = "test2";
-        System.out.println(String.format("%1$s and %2$s: %3$b", str1, str2, isPermutationSortedString("test1", "test2")));
+        System.out.println(String.format("%1$s and %2$s: %3$b", str1, str2, isPermutationSortedString(str1, str2)));
+        str1 = "dog";
+        str2 = "god";
+        System.out.println(String.format("%1$s and %2$s: %3$b", str1, str2, isPermutationSortedString(str1, str2)));
     }
 }
