41043840f4389151a395ee302b77c1a4ce1d7cc6
Darren Labey <dlabey@fastmail.com>
2018-12-02 17:41:14 -0800

    Making a Maven project and adding tests files for tests

---
+++ src/test/java/org/crackingthecodinginterview/chapter01/checkPermutation/MainTest.java
@@ -0,0 +1,4 @@
+package test.java.org.crackingthecodinginterview.chapter01.checkPermutation;
+
+public class MainTest {
+}
---
+++ src/test/java/org/crackingthecodinginterview/chapter01/isUnique/MainTest.java
@@ -0,0 +1,4 @@
+package test.java.org.crackingthecodinginterview.chapter01.isUnique;
+
+public class MainTest {
+}
