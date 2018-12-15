1b2fd62c2266be8c2c24e83775b9602e2d2a9d31
Darren Labey <darren.labey@am.sony.com>
2018-12-14 17:57:56 -0800

    Better solution for urlify

--- src/main/java/org/crackingthecodinginterview/chapter01/urlify/Main.java
+++ src/main/java/org/crackingthecodinginterview/chapter01/urlify/Main.java
@@ -1,19 +1,56 @@
 package org.crackingthecodinginterview.chapter01.urlify;
 
 class Main {
-    static char[] urlify(char[] str) {
-        // start from the end and this assumes fixed length like the book, if not do array copy
+    static char[] urlifyFixed(char[] str) {
+        // start from the end and this assumes fixed length like the book
         for (int i = 0; i < str.length; i++) {
-            if (str[i] == ' ') {
-                // shift right 2 spaces starting at the position after the original space until the array length is reached
-                for (int j = str.length - 2; j > i + 1; j = j - 2) {
-                    str[j] = str[j - 2];
-                    str[j + 1] = str[j - 1];
+            if (i + 1 > str.length - 1 || i + 2 > str.length - 1) {
+                return str;
+            } else {
+                if (str[i] == ' ' && str[i + 1] == ' ' && str[i + 2] == ' ') {
+                    // replace the space and the 2 chars after with %20
+                    str[i] = '%';
+                    str[i + 1] = '2';
+                    str[i + 2] = '0';
+                } else {
+                    if (str[i] == ' ') {
+                        char[] newStr = new char[str.length];
+                        int iLeftLength = i + 1;
+                        int iRight = i + 2;
+                        int iRightLength = str.length - iRight;
+
+                        System.arraycopy(str, 0, newStr, 0, iLeftLength);
+                        System.arraycopy(str, iLeftLength, newStr, iRight, iRightLength);
+
+                        newStr[i + 1] = ' ';
+
+                        return urlifyFixed(newStr);
+                    }
                 }
-                // replace the space and the 2 chars after with %20
-                str[i] = '%';
-                str[i + 1] = '2';
-                str[i + 2] = '0';
+            }
+        }
+
+        return str;
+    }
+
+    static char[] urlify(char[] str) {
+        // start from the end and this is unfixed length
+        for (int i = 0; i < str.length; i++) {
+            // if there is a space, create a new array with 2 more items and copy the existing array around them
+            if (str[i] == ' ') {
+                char[] newStr = new char[str.length + 2];
+                int iLeftLength = i + 1;
+                int iRight = i + 2;
+                int iRightLength = str.length - i;
+
+                System.arraycopy(str, 0, newStr, 0, iLeftLength);
+                System.arraycopy(str, i, newStr, iRight, iRightLength);
+
+                newStr[i] = '%';
+                newStr[i + 1] = '2';
+                newStr[i + 2] = '0';
+
+                str = newStr;
             }
         }
 
@@ -21,10 +58,13 @@ class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("urlify:");
+        System.out.println("urlifyFixed:");
         char[] str = "Mr John Smith    ".toCharArray();
         System.out.println(new String(str));
-        char[] str2 = urlify(str);
+        char[] str2 = urlifyFixed(str);
         System.out.println(new String(str2));
+        System.out.println("urlify:");
+        char[] str3 = urlify(str);
+        System.out.println(new String(str3));
     }
 }
--- src/test/java/org/crackingthecodinginterview/chapter01/urlify/MainTest.java
+++ src/test/java/org/crackingthecodinginterview/chapter01/urlify/MainTest.java
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.Test;
 
 class MainTest {
     @Test
+    void urlifyFixedShouldUrlifyFixedAssumedLengthString() {
+        char[] str = "Mr John Smith    ".toCharArray();
+        char[] strUrlified = Main.urlifyFixed(str);
+
+        Assertions.assertEquals("Mr%20John%20Smith", new String(strUrlified));
+    }
+
+    @Test
     void urlifyShouldUrlifyFixedAssumedLengthString() {
         char[] str = "Mr John Smith    ".toCharArray();
         char[] strUrlified = Main.urlify(str);
 
-        Assertions.assertEquals(new String(str), new String(strUrlified));
+        Assertions.assertEquals("Mr%20John%20Smith%20%20%20%20", new String(strUrlified));
     }
 }
