dbebd253970634a5132262a88c0ee8221f3f9384
Darren Labey <dlabey@fastmail.com>
2018-12-18 20:37:03 -0800

    One Away outline

---
+++ src/main/java/org/crackingthecodinginterview/chapter01/oneAway/Main.java
@@ -0,0 +1,14 @@
+package org.crackingthecodinginterview.chapter01.oneAway;
+
+public class Main {
+
+    // insert a char
+    // remove a char
+    // replace a char
+    // pale => ple = true, pales => pale = true, pale => bale = true, pale => bake = false
+    // hint: each condition separately?, what is the relationship between insert and remove? do they need to be checked
+    // separately? can you do all 3 checks in a single pass?
+    public static void main(String[] args) {
+
+    }
+}
