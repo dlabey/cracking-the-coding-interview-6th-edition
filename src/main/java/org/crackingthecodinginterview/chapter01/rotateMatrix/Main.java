dd01869da782b2124db453f2889214ae1c33ed1b
Darren Labey <dlabey@fastmail.com>
2019-01-02 14:03:06 -0800

    Matrix rotation solution needs cleanup

--- src/main/java/org/crackingthecodinginterview/chapter01/rotateMatrix/Main.java
+++ src/main/java/org/crackingthecodinginterview/chapter01/rotateMatrix/Main.java
@@ -2,11 +2,44 @@ package org.crackingthecodinginterview.chapter01.rotateMatrix;
 
 public class Main {
 
+    static boolean rotate(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length) return false; // Not a square
+        int n = matrix.length;
+
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+            for(int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i]; // save top
+
+                // left -> top
+                matrix[first][i] = matrix[last-offset][first];
+
+                // bottom -> left
+                matrix[last-offset][first] = matrix[last][last - offset];
+
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+
+                // top -> right
+                matrix[i][last] = top; // right <- saved top
+            }
+        }
+        return true;
+    }
+
     // 1 2 3    7 4 1
     // 4 5 6 => 8 5 2
     // 7 8 9    9 6 3
     // rotate by 90 degrees
     public static void main(String[] args) {
-
+        int[][] matrix = new int[][] {
+                { 1, 2, 3},
+                { 4, 5, 6},
+                { 7, 8, 9}
+        };
+        rotate(matrix);
+        System.out.println(matrix);
     }
 }
