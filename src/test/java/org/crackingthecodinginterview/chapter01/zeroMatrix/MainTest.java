b687dba668c73f3ac9430d050d5a746c2787ecbd
Darren Labey <dlabey@fastmail.com>
2020-03-03 21:21:59 -0800

    Test for zeroed matrix.

--- src/test/java/org/crackingthecodinginterview/chapter01/zeroMatrix/MainTest.java
+++ src/test/java/org/crackingthecodinginterview/chapter01/zeroMatrix/MainTest.java
@@ -1,4 +1,24 @@
 package org.crackingthecodinginterview.chapter01.zeroMatrix;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
+
+    @Test
+    void zeroShouldZeroMatrix() {
+        int[][] matrix = new int[][] {
+                { 1, 0, 3},
+                { 4, 5, 2},
+                { 0, 8, 9}
+        };
+        Main.zero(matrix);
+        int[][] zeroedMatrix = new int[][] {
+                { 0, 0, 0},
+                { 0, 0, 2},
+                { 0, 0, 0}
+        };
+
+        Assertions.assertEquals(Main.printMatrix(zeroedMatrix), Main.printMatrix(matrix));
+    }
 }
